60fce346077315988e30535e88ea5f8b9f2737b7
Arianna Quinci <72978793+ariannaquinci@users.noreply.github.com>
2022-06-20 10:25:30 +0200

    Delete alphabetic_sorting/alphabetic_sorting directory

--- alphabetic_sorting/alphabetic_sorting/src/main/java/MainClass.java
+++
@@ -1,6 +0,0 @@
-public class MainClass {
-    public static void main(String[] args){
-        String[] words= new String[]{"apple","Zorro","cake","banana"};
-        AlphabeticSorting.sorting(words);
-    }
-}
